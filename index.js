9b61744b153efc646429ed5a54f5d6ea86f4fb9d
swetashivaniltts <Sweta.Shivani@Ltts.com>
2019-06-03 14:11:35 +0530

    simple nodejs code

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var PORT = 7980;
+
+var fruits = [{
+    name: "Mango",
+    color: "Yellow"
+}, {
+    name: "Orange",
+    color: "Orange"
+}, {
+    name: "Watermelon",
+    color: "Green"
+}];
+
+app.get('/color', function (req, res) {
+    console.log('[INFO] - Request received for specific fruit');
+    var colorName = req.param('color');
+    console.log("color name: ", colorName);
+    res.end();
+});
+
+app.get('/fruits', function (req, res) {
+    console.log('[INFO] - Request received for listing fruits');
+    res.json(fruits);
+    res.end();
+});
+
+
+
+app.listen(PORT, () => {
+    console.log('[INFO] -- Server started and listening on: ', PORT);
+});
